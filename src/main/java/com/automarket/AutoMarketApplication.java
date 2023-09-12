280e0a3101764d3a7a888a20a4749e9c85f28db2
orestun <orest.uzhytchak0001@gmail.com>
2023-09-12 10:15:53 +0300

    Initial commit

---
+++ src/main/java/com/automarket/AutoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.automarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMarketApplication.class, args);
+    }
+
+}
